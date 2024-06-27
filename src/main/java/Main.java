18271d2ec6e0883ef6f796f88725bdc974d6cb6a
ttxlnguyen <ttxl.nguyen@gmail.com>
2024-06-27 18:22:41 -0400

    Finished

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,22 +7,24 @@ public class Main {
 
     public static void main(String[] args ){
         // create a new object of class Main
-
+        String name;
         // call the compute() method on that new object
-
+        compute();
     }
 
-    public void compute() {
+    public static void compute() {
         // Get input string of name from user
         Scanner in = new Scanner(System.in);
         System.out.println("Please enter your name");
         String name = in.nextLine();
 
         // Create conditional to check name against Alice and Bob
-
-        // Print greeting to screen if Alice or Bob are true
-        
+        if(name.equalsIgnoreCase("Alice") || name.equalsIgnoreCase("Bob")) {
+            System.out.println("Hello, " + name + "!");
+            // Print greeting to screen if Alice or Bob are true
+        } else {
+            System.out.println("You are neither Alice nor Bob.");
+        }
         // Print "You are neither Alice nor Bob." otherwise
-
     }
 }
