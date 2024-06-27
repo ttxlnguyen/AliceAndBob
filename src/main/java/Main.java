e3e575600e6a4ce0e28eb3146f2e9756f75493c3
ttxlnguyen <ttxl.nguyen@gmail.com>
2024-06-27 18:12:30 -0400

    Started

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,17 +2,22 @@
  * Created by iyasuwatts on 10/17/17.
  * Modified by kristofer on Jan 2024.
  */
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args ){
         // create a new object of class Main
 
         // call the compute() method on that new object
+
     }
 
     public void compute() {
         // Get input string of name from user
-        
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please enter your name");
+        String name = in.nextLine();
+
         // Create conditional to check name against Alice and Bob
 
         // Print greeting to screen if Alice or Bob are true
